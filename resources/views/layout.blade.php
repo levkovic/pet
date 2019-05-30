347008f6106dcbd8f27684afd51568d6e7c89a82
ilya.levkov1992@gmail.com <ilya.levkov1992@gmial.com>
2019-05-30 13:49:39 +0300

    the game link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,7 @@
 		<li><a href="/about">About</a></li>
 		<li><a href="/contact">Contact</a></li>
 		<li><a href="/projects">Projects</a></li>
+		<li><a href="/game">Game</a></li>
 	</ul>
 
 	@yield('content')
